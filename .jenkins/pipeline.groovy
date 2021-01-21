9c2cb2da8e1690d5c6334e9657b0f148c0379198
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2021-01-21 21:43:01 +0100

    Using a local Maven repository.

--- .jenkins/pipeline.groovy
+++ .jenkins/pipeline.groovy
@@ -12,8 +12,14 @@ pipeline {
         }
 
         stage ('Build') {
-            steps {
-				bat 'mvn.cmd -fJava/ispm-core/pom.xml -Dmaven.test.failure.ignore=true clean install'
+            withMaven(
+                 // Maven installation declared in the Jenkins "Global Tool Configuration"
+                 maven: 'Maven3',
+
+                // Use `$WORKSPACE/.repository` for local repository folder to avoid shared repositories
+                mavenLocalRepo: '.repository',
+            ) {
+			    bat 'mvn.cmd -fJava/ispm-core/pom.xml -Dmaven.test.failure.ignore=true clean install'
             }
         }
     }
